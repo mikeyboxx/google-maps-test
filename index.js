fbe4843dee972247d4c425277b5148df5e9bf681
Michael Nabatov <happyanddebtfree@gmail.com>
2022-12-20 17:42:46 -0500

    Added code to generate 10 markers within a random 1 mile radius

--- index.js
+++ index.js
@@ -1,36 +1,133 @@
 // Initialize and add the map
-function initMap() {
-  console.log(navigator);
-  const geoLocation = navigator.geolocation;
-  const position = geoLocation.getCurrentPosition(
-    (position)=>{
-      console.log(position);
-      const currentLocation = { lat: position.coords.latitude, lng: position.coords.longitude };
-  // The map, centered at Uluru
-      const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 18,
-        center: currentLocation,
-      });
-  // The marker, positioned at Uluru
-      const marker = new google.maps.Marker({
-        position: currentLocation,
-        map: map,
-      });
-    },
-    (error)=>{
-      console.warn(`ERROR(${error.code}): ${error.message}`);
-    },
-    {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0
-    }
-  );
-  // cobsole.log(position);
-
-  // The location of Uluru
-  // const uluru = { lat: -25.344, lng: 131.031 };
+function initMap3({lat, lng, mapContainer}) {
+  const mapEl = document.getElementById(mapContainer);
   
+  const map = new google.maps.Map(mapEl,
+    {
+      zoom: 14,
+      center: {lat, lng}
+    }
+    );
+    
+    return map;
+  }
+  
+  
+  function initMap() {
+    const position = navigator.geolocation.getCurrentPosition(
+      // callback with coordinates
+      (position)=>{
+        const currentLocation = { 
+          lat: position.coords.latitude, 
+          lng: position.coords.longitude 
+        };
+        const map = initMap3({...currentLocation, mapContainer: 'map'});
+
+        const userMarker = new google.maps.Marker({
+          position: currentLocation,
+          map: map,
+        });
+
+        const noteMarkers = [];
+
+        console.log(currentLocation);
+        // generate random notes within a radius
+        for (let i = 0; i < 10; i ++){
+          let n = (Math.floor(Math.random() * 100) + 1)/10000;
+          let posOrNeg = Math.round(Math.random());
+          const lat = posOrNeg ? currentLocation.lat + n : currentLocation.lat - n
+          
+          n = (Math.floor(Math.random() * 100) + 1)/10000;
+          posOrNeg = Math.round(Math.random());
+          const lng = posOrNeg ? currentLocation.lng + n : currentLocation.lng - n
+
+          const distance = getDistanceFromLatLonInKm(currentLocation.lat, currentLocation.lng, lat, lng )/1.609;
+
+          console.log(lat, lng, distance);
+          const pos = {
+            lat,
+            lng
+          }
+
+          const iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+
+          const marker = new google.maps.Marker({
+            position: {
+              ...pos
+            },
+            title: `Latitude: ${lat} | Longitude: ${lng}`,
+            map: map,
+            animation: google.maps.Animation.DROP,
+            icon: iconBase + "library_maps.png"
+          });
+
+
+          const contentString = `
+          <div>
+            <p>Latitude: ${lat.toFixed(3)}</p>
+            <p>Longitude: ${lng.toFixed(3)}</p>
+            <p>Distance: ${distance.toFixed(2)} miles</p>
+          </div>`;
+
+          const infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            ariaLabel: "pos",
+          });
+
+          marker.addListener("click", () => {
+            infowindow.open({
+              anchor: marker,
+              map,
+            });
+          });
+
+          noteMarkers.push(marker);
+        }
+  
+        // const timer = setInterval(()=>{
+        //   console.log('tick');
+        //   console.log(currentLocation);
+        //   currentLocation.lat += .01;
+        //   currentLocation.lng += .01;
+
+        //   let latlng = new google.maps.LatLng(currentLocation.lat, currentLocation.lng);
+        //   marker.setTitle("Latitude:" + currentLocation.lat +" | Longitude:" + currentLocation.lng);
+        //   marker.setPosition(latlng);
+
+        //   // marker.setPosition(currentLocation);
+        //   // marker.map = map;
+        // }, 3000)
+      },
+      (error) => console.warn(`ERROR(${error.code}): ${error.message}`),
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      }
+
+      
+    );
+  }
+
+
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
 }
 
-window.initMap = initMap;
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+
+// window.initMap = initMap;
+initMap();
+// x++;
